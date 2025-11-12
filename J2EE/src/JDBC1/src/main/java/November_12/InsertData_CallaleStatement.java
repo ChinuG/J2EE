1f3238f7743763b0f9e9425da37b92e1bd1368be
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2025-11-12 09:36:57 +0530

    CallableStatement
    example of Callable Statement for inserting data into database

---
+++ J2EE/src/JDBC1/src/main/java/November_12/InsertData_CallaleStatement.java
@@ -0,0 +1,52 @@
+package November_12;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class InsertData_CallaleStatement {
+	private static String URL = "jdbc:postgresql://localhost:5432/qsp";
+	private static String name = "postgres";
+	private static String pass = "root";
+	private static Scanner sc = new Scanner(System.in);
+	public static void main(String args[])
+	{
+		try
+		{
+//			1st Step - Load Or Register the Driver
+			Class.forName("org.postgresql.Driver");
+			System.out.println("Driver is Loaded");
+			
+//			2nd Step - Create Connection.
+			Connection con = DriverManager.getConnection(URL, name, pass);
+			System.out.println("Connected");
+			
+//			3rd Step - 
+			CallableStatement cs = con.prepareCall("call insert_student(?,?,?)");
+			System.out.println("Enter Student Info ");
+			System.out.print("Enter Id : ");
+			cs.setInt(1, sc.nextInt());
+			
+			sc.nextLine();
+			
+			System.out.print("Enter name : ");
+			cs.setString(2, sc.nextLine());
+			
+			System.out.print("Enter Phone No : ");
+			cs.setLong(3, sc.nextLong());
+			
+//			4th Step -
+			cs.execute();
+			
+//			5th Step -  Close Connection
+			con.close();
+//			System.out.println("Connection is Closed");)
+		}
+		catch(ClassNotFoundException | SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
