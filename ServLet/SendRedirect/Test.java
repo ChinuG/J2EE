f08d1cdc8066439f200e6d95f39359021095c0b0
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2026-02-16 23:19:53 +0530

    SendRedirect Servlet
    Example of sendRedirect in Servlet

---
+++ ServLet/SendRedirect/Test.java
@@ -0,0 +1,26 @@
+package com.jsp.servlet.send_redirect;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(value = "/Test")
+public class Test extends HttpServlet{
+	
+		
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+//		resp.sendRedirect("First.html");
+	
+		
+		PrintWriter pw = resp.getWriter();
+		pw.write("<h1> I am Test Servler Class </h1>");
+	}
+	
+}
