3f07949adb2fb2db943afa777a8cb4f5c697b3ad
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2026-02-17 23:52:28 +0530

    Third Java file
    Example of LoadOnStartUp Third.java file

---
+++ ServLet/LoadOnStartUp/Third.java
@@ -0,0 +1,44 @@
+package com.jsp.servlet.loadonstartup;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(value = "/Third", loadOnStartup = 1)
+public class Third implements Servlet{
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+
+		System.out.println("Third Servlet Class");
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.println("Third Servlet class Service() is executed..");
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	 
+}
