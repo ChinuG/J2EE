e57a889573b14bf3c0f1f2c1438904f45b06a136
Chinmay Gaikwad <97344345+ChinuG@users.noreply.github.com>
2026-02-17 23:26:51 +0530

    LoadOnStartUp
    Example of LoadOnStartUp

---
+++ ServLet/LoadOnStartUp/first.java
@@ -0,0 +1,45 @@
+package com.jsp.servlet.loadonstartup;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(value = "/first", loadOnStartup = 2)
+public class first implements Servlet{
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+
+		System.out.println("First servlet class");
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+		System.out.println("First servlet class Service() is executed..");
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
